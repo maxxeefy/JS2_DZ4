3ccc3e8e61c1e80f2bd31849dd85761c4f64b4f4
w-MAX-w <mxprojekt@gmail.com>
2019-08-20 23:13:19 +0300

    Выполнил задание к уроку 4

---
+++ 2/main.js
@@ -0,0 +1,8 @@
+const str = "One: 'Hi Mary.' Two: 'Oh, hi.' One: 'How are you doing?' Two: 'I'm doing alright.How about you ?' One: 'Not too bad. The weather is great isn't it ?' Two: 'Yes. It's absolutely beautiful today.' One: 'I wish it was like this more frequently.' Two: 'Me too.' One: 'So where are you going now?' Two: 'I'm going to meet a friend of mine at the department store.' One: 'Going to do a little shopping?' Two: 'Yeah, I have to buy some presents for my parents.' One: 'What's the occasion ? 'Two: 'It's their anniversary.' One: 'That's great.Well, you better get going.You don 't want to be late.' Two: 'I'll see you next time.' One: 'Sure. Bye.'"
+
+let regexpAllPoints = new RegExp('\'', 'g');
+let regexpReturnApostroph = /\b\"\b/g;
+let newstr = str.replace(regexpAllPoints, '"');
+newstr = newstr.replace(regexpReturnApostroph, '\'');
+
+console.log(newstr);
